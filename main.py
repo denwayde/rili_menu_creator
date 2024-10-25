efbe076125fbafd85d853836c51e765e6ce8288f
denwayde <denwayde09@gmail.com>
2024-10-25 16:07:36 +0500

    gggg

--- main.py
+++ main.py
@@ -9,19 +9,16 @@ osnova = ["Гречка", "Картофель отварной", "Макарон
 dni_nedeli = ["Понедельник", "Вторник", "Среда", "Чутверг", "Пятница", "Суббота", "Воскресенье"]
 
 obed = []
-i = 1
-while len(obed) < 7:
-    osnova_el = random.choice(osnova)
-    garnir_el = random.choice(garnir_obed)
-    if len(obed)!=0:
-        if osnova_el != obed[i-1][0] and garnir_el != obed[i-1][1]:
-            obed.append([osnova_el, garnir_el])
-            # osnova.remove(osnova_el)
-            i = i+1
-    else:
-        obed.append([osnova_el, garnir_el])
-    
+#i = 1
+for i in garnir_obed:
+    for x in osnova:
+        obed.append([i, x])
+
+
 print(obed)
-print(len(obed))
+
+# full_obed = obed+obed_plus
+# print(full_obed)
+# print(len(full_obed))
 
 
